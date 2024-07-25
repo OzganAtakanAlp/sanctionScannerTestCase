6690550bcdcf2384fbfa61e56ea799d24ca8623a
OzganAtakanAlp <atakanaozgan@gmail.com>
2024-07-25 19:13:00 +0300

    initial commit

--- SanctionScannerTestCase/Controllers/HomeController.cs
+++ SanctionScannerTestCase/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Oracle.ManagedDataAccess.Client;
 using SanctionScannerTestCase.Models;
 
 namespace SanctionScannerTestCase.Controllers;
@@ -7,6 +8,7 @@ namespace SanctionScannerTestCase.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private OracleConnection connection = new OracleConnection();
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -27,6 +29,80 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    //BOOKS TABLE'S COLUMNS: ID(INTEGER), NAME(VARCHAR2), AUTHOR(VARCHAR2), COVER(VARCHAR2), ISINLIBRARY(BOOLEAN)
+
+    //CRUD
+    //CREATE
+    public void CreateBook(string name, string author)
+    {
+        //couldn't create db connection, but here books will be created and added to the library db
+        Book p = new Book(name, author);
+
+        string sql = $"INSERT INTO BOOKS VALUES(${name}, ${author}, TRUE)";
+    }
+
+    //READ
+    public Book GetBookInfo(int id)
+    {
+        string sql = $"SELECT * FROM BOOKS WHERE ID LIKE %{id}%";
+
+        //here sql will be used and the desired info will be read
+
+        //Book p = new Book(name, author, cover, isBookInLibrary);
+
+        return new Book();
+    }
+
+    //UPDATE
+    public void UpdateBook(Book book)
+    {
+        string sql = $"UPDATE BOOKS SET AUTHOR = ${book.Author}";
+    }
+
+    public void UpdateBookName()
+    {
+        string sql = $"UPDATE BOOKS SET ";
+    }
+
+    public void UpdateBookAuthor()
+    {
+        string sql = $"UPDATE BOOKS SET ";
+    }
+
+    public void UpdateBookCover()
+    {
+        string sql = $"UPDATE BOOKS SET ";
+    }
+
+    public void UpdateBookAvailability()
+    {
+        string sql = $"UPDATE BOOKS  ";
+    }
+
+    //DELETE
+    public void DeleteBook(int bookID)
+    {
+        string sql = $"";
+    }
+
+
+    public bool BorrowBook(int bookID)
+    {
+        if (GetBookInfo(bookID).IsBookInLibrary)
+        {
+           return true; 
+        }
+
+        return false;
+    }
+
+    public Book ReturnInfo(int ID)
+    {
+        Book p;
+
+        return new Book();
     }
 }
 
---
+++ SanctionScannerTestCase/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SanctionScannerTestCase.Models
+{
+	public class Book
+	{
+
+		public string Name { get; set; }
+        public string Author { get; set; }
+        public string Cover  { get; set; }
+		public bool IsBookInLibrary { get; set; }
+
+
+        public Book()
+		{
+		}
+
+        public Book(string name, string author)
+        {
+        }
+
+        public Book(string name, string author, string cover, bool isBookInLibrary)
+        {
+
+        }
+
+    }
+}
+
